489f47d51171ef373d093f10cf1d5e5a75ca077e
Elena Prokhorova <eprokhorova@mail.ru>
2024-03-08 02:47:03 +0200

    change text

--- text.py
+++ text.py
@@ -7,9 +7,9 @@ main_menu = ['Главное меню',
              'Изменить контакт',
              'Удалить контакт',
              'Выход']
-
-coice_main_menu = f'Выберите пункт меню ({1} - {len(main_menu)-1})'
-coice_main_menu_error = f'Нужно ввести число от 1 до {len(main_menu)-1}!'
+ 
+choice_main_menu = f'Выберите пункт меню ({1} - {len(main_menu)-1}): '
+choice_main_menu_error = f'Нужно ввести число от 1 до {len(main_menu)-1}!'
 phone_book_opened_successful = 'Телефонная книга открыта успешно!'
 phone_book_saved_successful = 'Телефонная книга сохранена успешно!'
 empty_phone_book_error = 'Телефонная книга пуста или не открыта!'
@@ -23,8 +23,8 @@ edit_contact=[f'Введите новое имя ({no_changes}): ',
               f'Введите новый комментарий ({no_changes}): ',]
 
 input_search_word = 'Введите слово для поиска: '
-input_serach_word_for_edit = 'Введите слово для поиска контакта, который хотите изменить: '
-input_serach_word_for_delete = 'Введите слово для поиска контакта, который хотите удалить: '
+input_search_word_for_edit = 'Введите слово для поиска контакта, который хотите изменить: '
+input_search_word_for_delete = 'Введите слово для поиска контакта, который хотите удалить: '
 input_id_for_edit = 'Введите ID контакта, который хотите изменить: '
 input_id_for_delete = 'Введите ID контакта, который хотите удалить: '
 
