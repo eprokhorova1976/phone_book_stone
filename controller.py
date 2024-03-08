0d572f6e9de06deedd97a51608242345dc422a6e
Elena Prokhorova <eprokhorova@mail.ru>
2024-03-08 02:53:07 +0200

    change controller

--- controller.py
+++ controller.py
@@ -4,7 +4,7 @@ import text
 
 def find_contacts(message):
     search_word = view.input_data (message)
-    result = model.find_contacts (search_word)
+    result = model.find_contact (search_word)
     view.show_contacts (result, text.find_contact_no_result(search_word))
     return True if result else False
 
@@ -40,3 +40,4 @@ def start_app():
             case 8:
                 break
             
+
