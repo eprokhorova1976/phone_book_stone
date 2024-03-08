7b02fb822a5906e7c37adbef5de9fd2bc17fac86
Elena Prokhorova <eprokhorova@mail.ru>
2024-03-08 02:48:03 +0200

    change view

--- view.py
+++ view.py
@@ -1,7 +1,7 @@
 import text
 THEME = '='
 
-def show_main_menu() ->int:
+def show_main_menu() -> int:
     for i, item in enumerate(text.main_menu):
         if i:
             print (f'\t{i}. {item}')
@@ -13,21 +13,21 @@ def show_main_menu() ->int:
             return int(choice)
         print (text.choice_main_menu_error)
 
-def show_contacts(phone_book: dict[int,[str]], error_message:str):
+def show_contacts(phone_book: dict[int, str], error_message: str):
     if phone_book:
         print('\n'+THEME*71)
         for u_id, contact in phone_book.items():
             print(f'{u_id:>3}. {contact[0]:<20} | {contact[1]:<20} | {contact[2]:<20}')
             print (THEME*71 + '\n')
-        else:
-            show_message(error_message)
+    else:
+        show_message(error_message)
 
-def show_message(message:str):
+def show_message(message: str):
     print ('\n'+THEME*len(message))
     print (message)
     print (THEME*len(message)+'\n')
 
-def input_data(message) ->list[str] | str:
+def input_data(message) -> list[str] | str:
     if isinstance(message,str):
-        return input('\n'+message)
+        return input('\n' + message)
     return[input(mes) for mes in message]
